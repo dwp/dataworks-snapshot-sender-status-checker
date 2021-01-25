2fd01948c7d4984be17d33dec2c517f594007b2c
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-01-25 07:50:01 +0000

    Initial commit, adding githooks submodule

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# dataworks-repo-template-docker
+# dataworks-snapshot-sender-status-checker
 
-## Description
+## An AWS lambda which receives SQS messages and monitors and reports on the status of a snapshot sender run.
 
 This repo contains Makefile, and Dockerfile to fit the standard pattern.
 This repo is a base to create new Docker image repos, adding the githooks submodule, making the repo ready for use.
--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,16 @@
-resource "aws_ecr_repository" "dataworks-repo-template-docker" {
-  name = "dataworks-repo-template-docker"
+resource "aws_ecr_repository" "dataworks-snapshot-sender-status-checker" {
+  name = "dataworks-snapshot-sender-status-checker"
   tags = merge(
     local.common_tags,
-    { DockerHub : "dwpdigital/dataworks-repo-template-docker" }
+    { DockerHub : "dwpdigital/dataworks-snapshot-sender-status-checker" }
   )
 }
 
-resource "aws_ecr_repository_policy" "dataworks-repo-template-docker" {
-  repository = aws_ecr_repository.dataworks-repo-template-docker.name
+resource "aws_ecr_repository_policy" "dataworks-snapshot-sender-status-checker" {
+  repository = aws_ecr_repository.dataworks-snapshot-sender-status-checker.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
 output "ecr_example_url" {
-  value = aws_ecr_repository.dataworks-repo-template-docker.repository_url
+  value = aws_ecr_repository.dataworks-snapshot-sender-status-checker.repository_url
 }
