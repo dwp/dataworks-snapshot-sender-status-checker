7b2884744ab4d9a2258d304cf20e173b734225b1
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-07-22 12:13:56 +0000

    Fixup: Format Python code with Black

--- src/status_checker_lambda/status_checker.py
+++ src/status_checker_lambda/status_checker.py
@@ -882,7 +882,11 @@ def is_collection_received(
     )
 
     is_received = (
-        collection_status in [EXPORTED_STATUS_VALUE, SENT_STATUS_VALUE]
+        collection_status
+        in [
+            EXPORTED_STATUS_VALUE,
+            SENT_STATUS_VALUE,
+        ]
         and int(collection_files_received_count) >= int(collection_files_exported_count)
     )
 
